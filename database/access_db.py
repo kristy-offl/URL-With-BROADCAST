f10798500594a872e6379f087749eb5740973b09
Rhodes Rhody <85724542+RhodesRhody@users.noreply.github.com>
2021-07-23 12:44:38 +0530

    Update access_db.py

--- database/access_db.py
+++ database/access_db.py
@@ -3,6 +3,6 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-from database.database import database
+from database.mongodb import database
 
 db = Database(Config.MONGODB_URI, Config.SESSION_NAME)
