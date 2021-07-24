6186b796884b76d98ba7288e4552f6943b0e70e3
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-24 10:13:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,7 +13,84 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 async def start(bot, update):
     await AddUserToDatabase(bot, update)
     await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
+        text=mtb.START_TXT.format(update.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup=Translation.START_BUTTONS
+        reply_markup=mtb.START_BUTTONS
     )
+
+@Client.on_message(filters.command(["help"])) & filters.private)
+async def help_txt(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=mtb.HELP_TXT.format(update.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                 [
+                     InlineKeyboardButton('⚙️ 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍', url='https://t.me/HiroshiBots')
+                ],
+                [
+                    InlineKeyboardButton('📍 𝑺𝒖𝒑𝒑𝒐𝒓𝒕 𝑮𝒓𝒐𝒖𝒑 ', url='https://t.me/HiroshiBotsSupport'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
+                    InlineKeyboardButton('🙋🏻 𝑫𝒆𝒗𝒐𝒍𝒐𝒑𝒆𝒓 ', url='https://t.me/Abt_Meh') # Must Give us Credits!
+                ]
+            ]
+        ),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+    
+@Client.on_message(filters.command(["stats"]))
+async def stats_text(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.STATS_TEXT,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                 [
+                     InlineKeyboardButton('⚙️ 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍', url='https://t.me/HiroshiBots')
+                 ]
+             ]
+         ),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+@Client.on_message(filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                 [
+                     InlineKeyboardButton('𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐃𝐞𝐯𝐨𝐥𝐨𝐩𝐞𝐫', url='https://t.me/Itz_Me_Malayali')
+                 ]
+             ]
+         ),
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["about"]))
+async def about_text(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT_TEXT,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                 [
+                     InlineKeyboardButton('⚙️ 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍', url='https://t.me/HiroshiBots')
+                ],
+                [
+                    InlineKeyboardButton('📍 𝑺𝒖𝒑𝒑𝒐𝒓𝒕 𝑮𝒓𝒐𝒖𝒑 ', url='https://t.me/HiroshiBotsSupport'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
+                    InlineKeyboardButton('🙋🏻 𝑫𝒆𝒗𝒐𝒍𝒐𝒑𝒆𝒓 ', url='https://t.me/Abt_Meh') # Must Give us Credits!
+                ]
+            ]
+        ),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )    
