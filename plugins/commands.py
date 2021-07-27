c33eea28c13fa49497984280a63ed4a51878a669
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-27 11:45:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,27 +47,11 @@ async def help_txt(bot, update):
 async def stats_text(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
-        text=
-    currentTime = get_readable_time(time.time() - botStartTime)
-    total, used, free = shutil.disk_usage('.')
-    total = get_readable_file_size(total)
-    used = get_readable_file_size(used)
-    free = get_readable_file_size(free)
-    sent = get_readable_file_size(psutil.net_io_counters().bytes_sent)
-    recv = get_readable_file_size(psutil.net_io_counters().bytes_recv)
-    cpuUsage = psutil.cpu_percent(interval=0.5)
-    memory = psutil.virtual_memory().percent
-    disk = psutil.disk_usage('/').percent
-    stats = f'<b>Bot Uptime:</b> {currentTime}\n' \
-            f'<b>Total Disk Space:</b> {total}\n' \
-            f'<b>Used:</b> {used}  ' \
-            f'<b>Free:</b> {free}\n\n' \
-            f'📊Data Usage📊\n<b>Upload:</b> {sent}\n' \
-            f'<b>Download:</b> {recv}\n\n' \
-            f'<b>CPU:</b> {cpuUsage}%\n' \
-            f'<b>RAM:</b> {memory}%\n' \
-            f'<b>DISK:</b> {disk}%'
-   )
+        text=Translation.STATS_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+      )
 
 @Client.on_message(filters.command(["upgrade"]) & filters.private)
 async def upgrade(bot, update):
