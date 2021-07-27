97621d9590b4d19a0d7e68f2e168ee069bbe6a82
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-27 10:48:05 +0530

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -2,8 +2,8 @@ import shutil, psutil
 import signal
 import os
 
-from status import status, dispatcher, updater, botStartTime
-from status import status, get_readable_file_size, get_readable_time
+import  dispatcher, updater, botStartTime
+import status, get_readable_file_size, get_readable_time
 
 def stats(update, context):
     currentTime = get_readable_time(time.time() - botStartTime)
