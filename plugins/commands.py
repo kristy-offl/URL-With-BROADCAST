e8bc43b01d9fdd73f3fdf106fa9bee3c87e59c5f
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-27 10:57:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,3 +1,5 @@
+import shutil, psutil
+import signal
 import os
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
@@ -44,7 +46,28 @@ async def help_txt(bot, update):
 async def stats_text(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.STATS_TEXT,
+        text="""
+def stats(update, context):
+    currentTime = get_readable_time(time.time() - botStartTime)
+    total, used, free = shutil.disk_usage('.')
+    total = get_readable_file_size(total)
+    used = get_readable_file_size(used)
+    free = get_readable_file_size(free)
+    sent = get_readable_file_size(psutil.net_io_counters().bytes_sent)
+    recv = get_readable_file_size(psutil.net_io_counters().bytes_recv)
+    cpuUsage = psutil.cpu_percent(interval=0.5)
+    memory = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+    stats = f'<b>Bot Uptime:</b> {currentTime}\n' \
+            f'<b>Total Disk Space:</b> {total}\n' \
+            f'<b>Used:</b> {used}  ' \
+            f'<b>Free:</b> {free}\n\n' \
+            f'📊Data Usage📊\n<b>Upload:</b> {sent}\n' \
+            f'<b>Download:</b> {recv}\n\n' \
+            f'<b>CPU:</b> {cpuUsage}%\n' \
+            f'<b>RAM:</b> {memory}%\n' \
+            f'<b>DISK:</b> {disk}%'
+    sendMessage(stats, context.bot, update),
         reply_markup=InlineKeyboardMarkup(
             [
                  [
