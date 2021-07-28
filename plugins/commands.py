9dbe4859b4fecf8a73b6f637984c75956c34b37b
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-28 13:07:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,16 +42,6 @@ async def help_txt(bot, update):
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
        )
-    
-@Client.on_message(filters.command(["stats"]) & filters.private)
-async def stats_text(bot, update):
-    await bot.send_message(
-         chat_id=update.chat.id,
-         text="uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)),
-         parse_mode="html",
-         reply_to_message_id=update.message_id,
-         disable_web_page_preview=True
-        )
 
 @Client.on_message(filters.command(["upgrade"]) & filters.private)
 async def upgrade(bot, update):
