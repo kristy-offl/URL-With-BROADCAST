0718177b1167375779d7d362d476854d5dd48334
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-28 13:25:18 +0530

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -6,14 +6,12 @@ import os
 from pyrogram import Client, filters
 
 bot_start_time = time.time()
-current_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%I:%M:%S %p')
-
 
 @Client.on_message(filters.command(["stats"]) & filters.private)
 async def stats_text(bot, update):
     await bot.send_message(
          chat_id=update.chat.id,
-         uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)),
+         await message.reply_text("uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)),
          parse_mode="html",
          reply_to_message_id=update.message_id,
          disable_web_page_preview=True
