451b14d25533165571dd52cdd08c574c4aadc2c1
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-29 09:43:27 +0530

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -7,11 +7,13 @@ from pyrogram import Client, filters
 
 bot_start_time = time.time()
 
+uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time,
+
 @Client.on_message(filters.command(["stats"]) & filters.private)
-async def stats_text(bot, update):
+async def stats (bot, update):
     await bot.send_message(
          chat_id=update.chat.id,
-         uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time,
+         text = uptime
          parse_mode="html",
          reply_to_message_id=update.message_id,
          disable_web_page_preview=True
