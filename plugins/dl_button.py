705725cfc4cf438cf1903bdb69e0e3edae181870
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-07-31 15:11:34 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -151,8 +151,7 @@ async def ddl_call_back(bot, update):
                     duration=duration,
                     # performer=response_json["uploader"],
                     # title=response_json["title"],
-                    # reply_markup=InlineKeyboardMarkup(
-                    # [[InlineKeyboardButton=f'📢 Join My Updates Channel 😄',url='https://t.me/HiroshiBots']]),
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('📢 Join My Updates Channel 😄', url='https://telegram.me/HiroshiBots')]]),
                     thumb=thumb_image_path,
                     reply_to_message_id=update.message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
@@ -167,9 +166,8 @@ async def ddl_call_back(bot, update):
                     chat_id=update.message.chat.id,
                     document=download_directory,
                     thumb=thumb_image_path,
-                    caption=description,
-                    # reply_markup=InlineKeyboardMarkup(
-                    # [[InlineKeyboardButton=f'📢 Join My Updates Channel 😄',url='https://t.me/HiroshiBots']]),
+                    caption=custom_file_name,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('📢 Join My Updates Channel 😄', url='https://telegram.me/HiroshiBots')]]),
                     reply_to_message_id=update.message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
@@ -202,8 +200,7 @@ async def ddl_call_back(bot, update):
                     width=width,
                     height=height,
                     supports_streaming=True,
-                    # reply_markup=InlineKeyboardMarkup(
-                    # [[InlineKeyboardButton=f'📢 Join My Updates Channel 😄',url='https://t.me/HiroshiBots']]),
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('📢 Join My Updates Channel 😄', url='https://telegram.me/HiroshiBots')]]),
                     thumb=thumb_image_path,
                     reply_to_message_id=update.message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
